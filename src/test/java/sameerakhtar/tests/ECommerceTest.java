268a2b4147edcea807e1af8ec6cd297387bf2761
Sameer <akhtarsameer743@gmail.com>
2025-04-02 02:20:49 +0530

    Adding Base Framework

---
+++ src/test/java/sameerakhtar/tests/ECommerceTest.java
@@ -0,0 +1,16 @@
+package sameerakhtar.tests;
+
+import org.testng.annotations.Test;
+
+import sameerakhtar.TestComponents.BaseTest;
+
+public class ECommerceTest extends BaseTest {
+
+	/**
+	 * 
+	 */
+	@Test
+	public void fillForm() {
+		
+	}
+}
