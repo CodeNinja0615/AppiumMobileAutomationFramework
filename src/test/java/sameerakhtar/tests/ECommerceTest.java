d9ffec7546b4656cb3c3f35fee0929d95c67986d
Sameer <akhtarsameer743@gmail.com>
2025-04-04 04:31:19 +0530

    Added Selenium Support dependency for explicit wait and added logic to validate cart product and total price

--- src/test/java/sameerakhtar/tests/ECommerceTest.java
+++ src/test/java/sameerakhtar/tests/ECommerceTest.java
@@ -1,9 +1,12 @@
 package sameerakhtar.tests;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -17,6 +20,7 @@ public class ECommerceTest extends BaseTest {
 	 */
 	@Test
 	public void endToEndTest() {
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		String[] expectedProduct = { "Air Jordan 4 Retro", "Jordan Lift Off" };
 		List<String> expectedProductList = Arrays.asList(expectedProduct);
 		driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/spinnerCountry")).click();
@@ -41,6 +45,25 @@ public class ECommerceTest extends BaseTest {
 			}
 		}
 		driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
+		WebElement cartPage = driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Cart']"));
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.visibilityOf(cartPage));
+		double sum = 0.0;
+		List<WebElement> cartProducts = driver.findElements(AppiumBy.xpath(
+				"//android.support.v7.widget.RecyclerView[@resource-id='com.androidsample.generalstore:id/rvCartProductList']/android.widget.RelativeLayout/android.widget.LinearLayout"));
+		for (WebElement cartProduct : cartProducts) {
+			String productName = cartProduct.findElement(AppiumBy.xpath("//android.widget.TextView")).getText();
+			Assert.assertTrue(expectedProductList.contains(productName));
+			String amountStr = cartProduct
+					.findElement(AppiumBy.xpath("//android.widget.LinearLayout[2]/android.widget.TextView")).getText()
+					.replace("$", "");
+			double intAmount = Double.parseDouble(amountStr);
+			sum += intAmount;
+		}
+		String totalAmountStr = driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/totalAmountLbl"))
+				.getText().replace("$", "");
+		double totalAmount = Double.parseDouble(totalAmountStr);
+		Assert.assertEquals(sum, totalAmount);
 		driver.findElement(AppiumBy.xpath("//android.widget.CheckBox")).click();
 		driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/btnProceed")).click();
 	}
