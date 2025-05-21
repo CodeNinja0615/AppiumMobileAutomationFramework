8e59fd544e755e97d7cdac798881e60d783d1e31
CodeNinja0615 <akhtarsameer743@gmail.com>
2025-05-21 22:28:28 +0530

    Adding Extent report and TestNGListener

---
+++ src/test/java/sameerakhtar/testUtils/ExtentReporterNG.java
@@ -0,0 +1,21 @@
+package sameerakhtar.testUtils;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReporterNG {
+
+	static ExtentReports extent;
+
+	public static ExtentReports getReporterObj() {
+		String path = System.getProperty("user.dir") + "//reports//index.html";
+		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+		reporter.config().setReportName("Web Automation Result");
+		reporter.config().setDocumentTitle("Test Results");
+
+		extent = new ExtentReports();
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("Tester", "Sameer Akhtar");
+		return extent;
+	}
+}
